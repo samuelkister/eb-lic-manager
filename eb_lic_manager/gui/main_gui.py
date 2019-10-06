d023beeb3a3ef79368a7efc20ddd6f4413dc6237
Kister Samuel <samuel.kister@gmail.com>
2019-10-06 17:26:32 +0200

    Begin GUI

---
+++ eb_lic_manager/gui/main_gui.py
@@ -0,0 +1,8 @@
+
+# Todo : display list of used licences, grouped by licenses or users
+# Todo :  + get list of avaiable licences -> need reader of lexLm config file (WIP)
+# Todo :  + get list of users -> need binding to Window User (TBD)
+# Todo :  + get list of used licences -> need bindings to FlexLm server API (TBD)
+# Todo :  + aggregate all lists (TBD)
+# Todo :  + display aggregated list -> In form of a tree? (TBD)
+
